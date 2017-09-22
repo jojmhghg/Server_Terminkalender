e81d9aa4156f3f67cc651918175e392bdd2d1aa3
TimMeyer <TimMeyer@DESKTOP-5P6KN64.fritz.box>
2017-09-22 10:27:44 +0200

    bugfixes vom 21.09 ca 16 uhr wieder hinzugefügt + jar hinzugefügt!

--- Server_Terminkalender/src/Terminkalender/DBHandler.java
+++ Server_Terminkalender/src/Terminkalender/DBHandler.java
@@ -264,7 +264,7 @@ public class DBHandler {
         PreparedStatement prepChangeTerminende = con.prepareStatement("UPDATE termin SET to_hours = ?, to_minutes = ? WHERE terminID = ?");
         prepChangeTerminende.setInt(1, neuesEnde.getStunde());
         prepChangeTerminende.setInt(2, neuesEnde.getMinute());
-        prepChangeTerminende.setInt(2, terminID);
+        prepChangeTerminende.setInt(3, terminID);
         prepChangeTerminende.execute(); 
     }
     
@@ -272,7 +272,7 @@ public class DBHandler {
         PreparedStatement prepChangeTerminbeginn = con.prepareStatement("UPDATE termin SET from_hours = ?, from_minutes = ? WHERE terminID = ?");
         prepChangeTerminbeginn.setInt(1, neuerBeginn.getStunde());
         prepChangeTerminbeginn.setInt(2, neuerBeginn.getMinute());
-        prepChangeTerminbeginn.setInt(2, terminID);
+        prepChangeTerminbeginn.setInt(3, terminID);
         prepChangeTerminbeginn.execute(); 
     }
     
@@ -280,8 +280,8 @@ public class DBHandler {
         PreparedStatement prepChangeTermin = con.prepareStatement("UPDATE termin SET day = ?, month = ?, year = ? WHERE terminID = ?");
         prepChangeTermin.setInt(1, neuesDatum.getTag());
         prepChangeTermin.setInt(2, neuesDatum.getMonat());
-        prepChangeTermin.setInt(2, neuesDatum.getJahr());
-        prepChangeTermin.setInt(2, terminID);
+        prepChangeTermin.setInt(3, neuesDatum.getJahr());
+        prepChangeTermin.setInt(4, terminID);
         prepChangeTermin.execute(); 
     }
     
@@ -314,7 +314,7 @@ public class DBHandler {
     }
     
     public void addAnfrage(int anfrageID, int userID, int terminID, int absenderID) throws SQLException{
-        PreparedStatement prepAddAnfrage = con.prepareStatement("INSERT INTO meldung values(?,?,?,?,?,?);");
+        PreparedStatement prepAddAnfrage = con.prepareStatement("INSERT INTO meldung values(?,?,?,?,?,?,?);");
         prepAddAnfrage.setInt(1, anfrageID);
         prepAddAnfrage.setInt(2, userID);
         prepAddAnfrage.setString(3, "");
